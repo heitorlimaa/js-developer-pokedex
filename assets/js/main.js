d28c159331b3ceb78a87aa9a0805883f1639c2db
Heitor Lima <heitorlima5252@gmail.com>
2023-09-18 22:24:17 -0300

    Resolução do Desafio DIO

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,52 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
-const limit = 10
-let offset = 0;
+const limit = 6
+let offset = 0
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+            <section class="header">
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                <span class="number">
+                    #${pokemon.number}
+                </span>
+
+                <span class="name">
+                    ${pokemon.name}
+                </span>
+
+                <div class="detail">
+
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+
+                </div>
+
+            </section>
+
+            <section class="info">
+
+                <div class="statsTitle">
+                    <span>
+                        Stats
+                    </span>
+
+                    <hr>
+                </div>
+
+                <ol class="stats">
+                    ${pokemon.stats.map((stat) => `<li class="stat">${stat}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
+            </section>
         </li>
     `
-}
+    }
 
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
